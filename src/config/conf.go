71aacd195f66c8c74362222eca7929ec195c290e
schoeu <schoeu89@gmail.com>
2018-04-19 15:51:03 +0800

    add gin & yaml config.

---
+++ src/config/conf.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"github.com/schoeu/golodash"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type Conf struct {
+	Port string `yaml:"port"`
+}
+
+var globalConfig *Conf
+
+func init() {
+	var conf Conf
+	globalConfig = conf.GetConfig()
+}
+
+func (c *Conf) GetConfig() *Conf {
+	confFileName := "config.yaml"
+	projectPath := filepath.Join(golodash.GetCwd(), "..", confFileName)
+	yamlFile, err := ioutil.ReadFile(projectPath)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return c
+}
+
+func GetConf() *Conf {
+	return globalConfig
+}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"./config"
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("test")
+	conf := config.GetConf()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + conf.Port)
 }
